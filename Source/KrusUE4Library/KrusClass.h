5e9ec4f9f4d8cd7ac8086035360fe0c92c12bbcb
SelfishKrus <selfishkrus@github.com>
2023-08-09 17:59:08 +0800

    no message

---
+++ Source/KrusUE4Library/KrusClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class KRUSUE4LIBRARY_API KrusClass
+{
+public:
+	KrusClass();
+	~KrusClass();
+};
---
+++ Source/KrusUE4Library/KrusUE4Library.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "KrusUE4Library.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KrusUE4Library, "KrusUE4Library" );
